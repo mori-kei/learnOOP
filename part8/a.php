f7d1d0b66edfeaa87dae37712f0c7bcf4ecdd8c0
mori-kei <morimeru13@gmail.com>
2024-05-27 15:46:49 +0900

    8. ポリモーフィズム（多態）

---
+++ part8/a.php
@@ -0,0 +1,120 @@
+
+<?php
+class Book
+{
+    private $title;
+    private $page_size;
+    public function __construct($title, $page_size)
+    {
+    $this->title = $title;
+    $this->page_size = $page_size;
+    }
+    public function getTitle()
+    {
+    return $this->title;
+    }
+    public function setTitle($value)
+    {
+    $this->title = $value;
+    }
+    public function getPageSize()
+    {
+    return $this->page_size;
+    }
+    public function setPageSize($value)
+    {
+    $this->page_size = $value;
+    }
+}
+
+
+
+// 本棚として本を格納するクラスの基底クラス
+class Bookshelf
+{
+  protected $books = []; // 子クラスで呼べるように protected
+
+  public function addBook($book)
+  {
+    // 自分自身（this）のcanAddBookメソッドを呼び出す
+    if (!$this->canAddBook($book)) return false;
+    array_push($this->books, $book);
+    return true;
+  }
+  public function findBookByTitle($title)
+  {
+    foreach ($this->books as $book) {
+      if ($book->getTitle() === $title) return $book;
+    }
+    return null;
+  }
+  public function sumPageSize()
+  {
+    $size = 0;
+    foreach ($this->books as $book) {
+      $size += $book->getPageSize();
+    }
+    return $size;
+  }
+  public function size()
+  {
+    return count($this->books);
+  }
+  public function canAddBook($book)
+  {
+    return true; 
+  }
+}
+
+class DebugBookshelf extends Bookshelf {
+    public function addBook($book)
+    {
+        $returnValue = parent::addBook($book);
+        echo "引数" ."\n";;
+        echo var_dump($book) . "\n";
+        echo "返り値" ."\n";
+        echo $returnValue . "\n";
+        echo var_dump($returnValue)  . "\n";
+        return $returnValue;
+    }
+    public function canAddBook($book)
+    {
+        $returnValue = parent::canAddBook($book);
+        echo "引数"."\n";
+        echo var_dump($book) . "\n";
+        echo "返り値" ."\n";
+        echo var_dump($returnValue)  . "\n";
+        return $returnValue;
+    }
+    public function findBookByTitle($title)
+    {
+        $returnValue = parent::findBookByTitle($title);
+        echo "引数"."\n";
+        echo var_dump($title) . "\n";
+        echo "返り値" ."\n";
+        echo var_dump($returnValue)   . "\n";
+    }
+}
+
+putenv("DEBUG=true");
+
+function createBookShelf(){
+    if(getenv('DEBUG') == 'true'){
+        return new DebugBookshelf;
+    }else{
+        return new Bookshelf;
+    }
+}
+
+$bookshelf = createBookShelf();
+$bookshelf->addBook(new Book("坊ちゃん", 520));
+$bookshelf->addBook(new Book("我輩は猫である", 454));
+$bookshelf->addBook(new Book("こころ", 876));
+
+if (!$bookshelf->addBook(new Book("門", 345))) {
+    echo "新しい本を追加できませんでした。今の本の数: " . $bookshelf->size() . "\n";
+}
+
+print_r($bookshelf->findBookByTitle("こころ"));
+echo $bookshelf->sumPageSize();
+echo "\n";
---
+++ part8/b.php
@@ -0,0 +1,91 @@
+<?php
+
+class Item{
+    private $price;
+    private $productName;
+    
+    public function __construct($productName,$price)
+    {
+        if ($price < 0) {
+            throw new InvalidArgumentException("価格は0以上にしてください");
+        }
+        if (empty($productName)) {
+            throw new InvalidArgumentException("商品名は空にできません。");
+        }
+        
+        $this->price = $price;
+        $this->productName = $productName;
+    }
+    
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    
+    public function getProductName()
+    {
+        return $this->productName;
+    }
+}
+
+class VendingMachine {
+    private $items = [];
+
+    public function addItem(Item $item,int $count){
+        for($i =0; $i < $count;$i++){
+            array_push($this->items,$item);
+        }
+    }
+    public function buy($productName,$cash){
+        if (!$this->canBuy($productName)) {
+            throw new InvalidArgumentException("在庫がありません");
+        }
+        //cashがpriceより高いことを確認
+        $item = $this->findItemByProductName($productName);
+        if ($item->getPrice() > $cash) {
+            throw new InvalidArgumentException("金額が不足しています");
+        }
+        unset($this->items[array_search($item, $this->items)]);
+        return $item;
+    }
+    public function canBuy($productName): bool{
+        //あとでPHPの配列関数に書き直す
+        foreach ($this->items as $item) {
+            if ($item->getProductName() === $productName) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function findItemByProductName($productName){
+        foreach($this->items as $item){
+            if($item->getProductName() ===$productName)return $item;
+        }
+        return null;
+    }
+
+    public function getItems()
+    {
+        return $this->items;
+    }
+}
+
+
+$item1 = new Item("コカコーラ",120);
+$item2 = new Item("綾鷹",100);
+$item3 = new Item("ウーロン茶",500);
+
+$vendingMachine = new VendingMachine();
+
+//商品（Item）を複数入れる事ができる。
+$vendingMachine->addItem($item1,3);
+$vendingMachine->addItem($item2,1);
+$vendingMachine->addItem($item3,1);
+echo var_dump($vendingMachine->getItems()); //コカコーラが3件、綾鷹,ウーロン茶が１件ずつ返ってくる
+
+//商品を購入
+$vendingMachine->buy("コカコーラ",120); //(正常系)
+$vendingMachine->buy("綾鷹",110); //(正常系)
+$vendingMachine->buy("コカコーラ",119); //(異常系)　cashがItemの価格を下回る場合
+$vendingMachine->buy("綾鷹",110); //(異常系)　在庫が0件
